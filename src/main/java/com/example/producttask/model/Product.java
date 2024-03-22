6b593d4ad9050889230999b9c52f784db01f8bde
Shivakant6387 <shivakantsingh900@gmail.com>
2024-03-22 10:27:03 +0530

    fixed issue

--- src/main/java/com/example/producttask/model/Product.java
+++ src/main/java/com/example/producttask/model/Product.java
@@ -1,11 +1,13 @@
 package com.example.producttask.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
 
-import javax.persistence.*;
 
 @Entity
 @AllArgsConstructor
